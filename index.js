499cec59e18dfb6abccd212879e549c863be754d
AsadurRubel <miarubel04@.com>
2024-05-06 17:33:28 +0600

    Finally complete the project

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5174", 
+    "http://localhost:5173",
+    "cars-doctor-e91ad.web.app",
+    "cars-doctor-e91ad.firebaseapp.com"
+  ],
     credentials: true,
   })
 );
@@ -37,7 +41,7 @@ const logger = async (req, res, next) => {
 };
 
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
+  const token = req?.cookies?.token;
   console.log("value of token in middleware", token);
   if (!token) {
     return res.status(401).send({ message: "not authorized" });
@@ -56,10 +60,17 @@ const verifyToken = async (req, res, next) => {
   
 };
 
+
+const cookieOptions = { 
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection = client.db("carDoctors").collection("service");
     const bookingCollection = client.db("carDoctors").collection("bookings");
@@ -71,14 +82,16 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
-        .send({ success: true });
+      res.cookie("token", token, cookieOptions).send({ success: true });
     });
+
+    app.post('/logout', async(req, res)=>{
+      const user = req.body;
+      console.log('logging out', user)
+      res.clearCookie('token', {...cookieOptions, maxAge:0}).send({success: true})
+    })
+
+
 
     // Services Related api
     app.get("/service", logger, async (req, res) => {
@@ -103,6 +116,7 @@ async function run() {
 
     app.get("/bookings", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
+      console.log('Cook cook cookies', req.user)
       // console.log('tok tok token', req.cookies.token)
       console.log('user in the valid token', req.user)
       if(req.query.email !== req.user.email){
@@ -145,7 +159,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
